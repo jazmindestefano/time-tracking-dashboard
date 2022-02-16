c757b9fb63040bdcd7cea872be46e37b964c4bdf
Jazmin <you@example.com>
2022-02-16 03:34:04 -0300

    window load changes

---
+++ main.js
@@ -0,0 +1,10 @@
+"use script"
+
+window.addEventListener('load', () => {
+    alert("Bienvenido to DashBoard!");
+
+    const work_hours = document.querySelector(".work-hours");
+    const input_value = input.value;
+   
+    input_value = "0Hrs";
+});
