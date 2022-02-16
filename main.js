c99379d540f7ab1d350616fc5ac2a20b627c6385
Jazmin <you@example.com>
2022-02-16 04:01:32 -0300

    commit style

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 "use script"
 
 window.addEventListener('load', () => {
-    alert("Bienvenido to DashBoard!");
 
     const work_hours = document.querySelector(".work-hours");
     const input_value = input.value;
